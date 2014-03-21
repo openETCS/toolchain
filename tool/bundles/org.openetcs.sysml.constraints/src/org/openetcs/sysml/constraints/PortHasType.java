e5bfb2da6f90f70032b8127fa9c711598ed33d70
Alexander Stante <alexander.stante@esk.fraunhofer.de>
2014-03-21 10:57:05 +0100

    Add constraint check for port type and remove the test for parameter type

--- tool/bundles/org.openetcs.sysml.constraints/src/org/openetcs/sysml/constraints/PortHasType.java
+++ tool/bundles/org.openetcs.sysml.constraints/src/org/openetcs/sysml/constraints/PortHasType.java
@@ -4,13 +4,14 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.validation.AbstractModelConstraint;
 import org.eclipse.emf.validation.IValidationContext;
 import org.eclipse.uml2.uml.Parameter;
+import org.eclipse.uml2.uml.Port;
 
-public class ParameterHasType extends AbstractModelConstraint {
+public class PortHasType extends AbstractModelConstraint {
 	public IStatus validate(IValidationContext ctx) {
-		Parameter parameter = (Parameter) ctx.getTarget();
+		Port parameter = (Port) ctx.getTarget();
 		
 		if (parameter.getType() == null) {
-			return ctx.createFailureStatus("The Parameter '" + parameter.getName() + "' has no associated type");
+			return ctx.createFailureStatus("The Port '" + parameter.getName() + "' has no associated type");
 		}
 		
 		return ctx.createSuccessStatus();
