9b9c3fcc8941e6e3fcef40f6c8e9796bc93a1b67
Alexander Stante <alexander.stante@esk.fraunhofer.de>
2014-07-30 17:20:24 +0200

    Add basic transformation functionality

--- tool/bundles/org.openetcs.sysml2scade.suite.transformation/src/org/openetcs/sysml2scade/suite/transformation/Transformation.java
+++ tool/bundles/org.openetcs.sysml2scade.suite.transformation/src/org/openetcs/sysml2scade/suite/transformation/Transformation.java
@@ -2,7 +2,16 @@ package org.openetcs.sysml2scade.suite.transformation;
 
 import java.io.File;
 
+import com.esterel.project.api.Project;
+import com.esterel.scade.api.OperatorKind;
+import com.esterel.scade.api.Package;
+import com.esterel.scade.api.ScadeFactory;
+import com.esterel.scade.api.ScadePackage;
+
 import org.eclipse.core.resources.IProject;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.uml2.uml.Model;
 
 public class Transformation {
@@ -15,8 +24,17 @@ public class Transformation {
 	}
 
 	public void generateAndWrite() {
-		// TODO Auto-generated method stub
+		ResourceSet resourceSet = new ResourceSetImpl();
+		URI baseURI = URI.createFileURI(project.getLocation().toOSString());
+		URI projectURI = baseURI.appendSegment(sysmlModel.getName() + ".etp");
 		
+		Project project = MapToScade.createEmptyScadeProject(projectURI, resourceSet);
+		Package mainModel = MapToScade.loadModel(projectURI, resourceSet);
+		MapToScade.fillScadeModel(mainModel, sysmlModel, baseURI);
+		
+		MapToScade.updateProjectWithModelFiles(project);
+		
+		MapToScade.saveAll(project, null);
 	}
 
 }
