7ffb9ef1f02965576489ce357a070565b23d0688
Matthieu PERIN <matthieu.perin@cea.fr>
2014-11-27 14:18:36 +0100

    First version of UI plugin for sysml2B transformation

---
+++ tool/bundles/org.openetcs.sysml2b.ui/src/org/openetcs/sysml2b/ui/handlers/DoThings.java
@@ -0,0 +1,28 @@
+package org.openetcs.sysml2b.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class DoThings extends AbstractHandler{
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return super.isEnabled();
+	}
+	
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		// do things
+		
+		
+		
+		
+		return event;
+
+	}
+
+
+}
