1e12575f4ecbff26cca569a5ce4aa578a0641c12
David MENTRE <d.mentre@fr.merce.mee.com>
2015-01-28 17:31:33 +0100

    Name better SysML2B transformation class. Update plugin.xml accordingly

--- tool/bundles/org.openetcs.sysml2b.ui/src/org/openetcs/sysml2b/ui/handlers/TransformToClassicalB.java
+++ tool/bundles/org.openetcs.sysml2b.ui/src/org/openetcs/sysml2b/ui/handlers/TransformToClassicalB.java
@@ -4,25 +4,18 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
-public class DoThings extends AbstractHandler{
-
+public class TransformToClassicalB extends AbstractHandler {
+	
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
 		return super.isEnabled();
 	}
-	
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		// do things
-		
-		
-		
-		
-		return event;
-
+		System.out.println(event);
+		return null;
 	}
-
 
 }
