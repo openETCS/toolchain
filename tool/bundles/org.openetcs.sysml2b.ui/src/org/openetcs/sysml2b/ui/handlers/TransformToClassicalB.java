f8e206890ba74a0e9489e27e6a254591ef0584c6
David MENTRE <d.mentre@fr.merce.mee.com>
2015-01-29 09:02:31 +0100

    First version of a SysML2B UI that calls Acceleo code

--- tool/bundles/org.openetcs.sysml2b.ui/src/org/openetcs/sysml2b/ui/handlers/TransformToClassicalB.java
+++ tool/bundles/org.openetcs.sysml2b.ui/src/org/openetcs/sysml2b/ui/handlers/TransformToClassicalB.java
@@ -1,5 +1,10 @@
 package org.openetcs.sysml2b.ui.handlers;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -8,6 +13,7 @@ import org.eclipse.papyrus.infra.emf.utils.EMFHelper;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.openetcs.sysml2b.transformation.common.main.M_MainSysMLTransform;
 
 public class TransformToClassicalB extends AbstractHandler {
 	
@@ -21,10 +27,25 @@ public class TransformToClassicalB extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
 		if (selection != null) {
+			// Extract model's EObject from event
 			IStructuredSelection structured_selection = (IStructuredSelection)selection;
 			EObject eobj = EMFHelper.getEObject(structured_selection.getFirstElement());
 			
-			System.out.println(eobj);
+			// Apply SysML2B transformation on EObject
+			List<String> arguments = new ArrayList<String>();
+			File folder = new File("/B_code/B_modules/");
+			
+			M_MainSysMLTransform transform;
+			try {
+				transform = new M_MainSysMLTransform(eobj, folder, arguments);
+				
+				System.out.println("** Start transformation **");
+				transform.doGenerate(null);
+				System.out.println("** End transformation **");
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		return null;
