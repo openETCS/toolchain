4507db893c95bf0c58e0917e96f7ada5b57531c8
David MENTRE <d.mentre@fr.merce.mee.com>
2015-01-29 08:13:29 +0100

    Get EObject from the command's event

--- tool/bundles/org.openetcs.sysml2b.ui/src/org/openetcs/sysml2b/ui/handlers/TransformToClassicalB.java
+++ tool/bundles/org.openetcs.sysml2b.ui/src/org/openetcs/sysml2b/ui/handlers/TransformToClassicalB.java
@@ -3,6 +3,11 @@ package org.openetcs.sysml2b.ui.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.papyrus.infra.emf.utils.EMFHelper;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 
 public class TransformToClassicalB extends AbstractHandler {
 	
@@ -14,7 +19,14 @@ public class TransformToClassicalB extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		System.out.println(event);
+		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
+		if (selection != null) {
+			IStructuredSelection structured_selection = (IStructuredSelection)selection;
+			EObject eobj = EMFHelper.getEObject(structured_selection.getFirstElement());
+			
+			System.out.println(eobj);
+		}
+		
 		return null;
 	}
 
