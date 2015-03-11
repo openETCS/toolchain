bececc7f1dd9a578560974917c2ac45e96dbc246
Matthias Zinner <matthias.zinner@esk.fraunhofer.de>
2015-03-11 16:45:07 +0100

    Change order of methods to be more logical

--- tool/bundles/org.openetcs.sysml2scade.suite.transformation/src/org/openetcs/sysml2scade/suite/transformation/Trace.java
+++ tool/bundles/org.openetcs.sysml2scade.suite.transformation/src/org/openetcs/sysml2scade/suite/transformation/Trace.java
@@ -66,10 +66,10 @@ public class Trace {
 
 	public Trace(final File file, final ErrorHandler handler)
 			throws ParserConfigurationException, SAXException, IOException {
-		this.sourceIDToTargetIDMap = new HashMap<>();
-		this.targetIDToSourceIDMap = new HashMap<>();
-		this.sourceIDToXmlElementMap = new HashMap<>();
-		this.pathToTargetElementsMap = new HashMap<>();
+		this.sourceIDToTargetIDMap = new HashMap<String, Set<String>>();
+		this.targetIDToSourceIDMap = new HashMap<String, Set<String>>();
+		this.sourceIDToXmlElementMap = new HashMap<String, Element>();
+		this.pathToTargetElementsMap = new HashMap<String, Element>();
 		this.documentFile = file;
 		DocumentBuilderFactory builderFactory = DocumentBuilderFactory
 				.newInstance();
@@ -102,58 +102,6 @@ public class Trace {
 		}
 	}
 
-	public List<String> getTargetLocations() {
-		return new ArrayList<String>(this.pathToTargetElementsMap.keySet());
-	}
-
-	private void loadElementsFromTarget(final Element trace) {
-		NodeList elementList = trace.getElementsByTagName(Trace.XML_ELEMENT);
-		for (int i = 0; i < elementList.getLength(); i++) {
-			Element element = (Element) elementList.item(i);
-
-			String sourceID = this.getSourceID(element);
-			this.sourceIDToXmlElementMap.put(sourceID, element);
-
-			for (Node node = element.getFirstChild(); node != null; node = node
-					.getNextSibling()) {
-				if (node.getNodeType() == Node.ELEMENT_NODE
-						&& ((Element) node).getTagName().equals(
-								Trace.XML_TARGET_ID)) {
-					String targetID = node.getTextContent();
-					this.storeIDs(sourceID, targetID);
-				}
-			}
-		}
-	}
-
-	public void addElement(final String sourceID, final String parentID,
-			final String targetID, final String... targetIDs) {
-
-		if (this.sourceIDToXmlElementMap.containsKey(sourceID)) {
-			throw new IllegalArgumentException(); // TODO
-		}
-		Element parent = (parentID != null && !parentID.equals("")) ? this.sourceIDToXmlElementMap
-				.get(parentID) : this.xmlTarget;
-		Element xmlElement = this.xmlDocument.createElement(Trace.XML_ELEMENT);
-		parent.appendChild(xmlElement);
-
-		Element xmlSource = this.xmlDocument.createElement(Trace.XML_SOURCE_ID);
-		xmlSource.setTextContent(sourceID);
-		xmlElement.appendChild(xmlSource);
-
-		Element xmlTarget = this.xmlDocument.createElement(Trace.XML_TARGET_ID);
-		xmlTarget.setTextContent(targetID);
-		xmlElement.appendChild(xmlTarget);
-		this.storeIDs(sourceID, targetID);
-		for (String target : targetIDs) {
-			xmlTarget = this.xmlDocument.createElement(Trace.XML_TARGET_ID);
-			xmlTarget.setTextContent(target);
-			xmlElement.appendChild(xmlTarget);
-			this.storeIDs(sourceID, target);
-		}
-		this.sourceIDToXmlElementMap.put(sourceID, xmlElement);
-	}
-
 	public void save() throws TransformerException {
 		this.xmlDocument.normalizeDocument();
 		TransformerFactory transformerFactory = TransformerFactory
@@ -180,39 +128,46 @@ public class Trace {
 		}
 	}
 
+	public List<String> getTargetLocations() {
+		return new ArrayList<String>(this.pathToTargetElementsMap.keySet());
+	}
+
 	public void removeTarget(final String projectName) {
 		Element element = this.pathToTargetElementsMap.remove(projectName);
 		element.getParentNode().removeChild(element);
 		this.xmlDocument.normalizeDocument();
 	}
 
-	public List<String> getTargetIDs(final String soureID) {
-		Collection<String> c = this.sourceIDToTargetIDMap.get(soureID);
-		if (c == null || c.size() == 0) {
-			return null;
+	public void addElement(final String sourceID, final String parentID,
+			final String targetID, final String... targetIDs) {
+
+		if (this.sourceIDToXmlElementMap.containsKey(sourceID)) {
+			throw new IllegalArgumentException(); // TODO
 		}
-		return new ArrayList<>(c);
+		Element parent = (parentID != null && !parentID.equals("")) ? this.sourceIDToXmlElementMap
+				.get(parentID) : this.xmlTarget;
+		Element xmlElement = this.xmlDocument.createElement(Trace.XML_ELEMENT);
+		parent.appendChild(xmlElement);
+
+		Element xmlSource = this.xmlDocument.createElement(Trace.XML_SOURCE_ID);
+		xmlSource.setTextContent(sourceID);
+		xmlElement.appendChild(xmlSource);
+
+		Element xmlTarget = this.xmlDocument.createElement(Trace.XML_TARGET_ID);
+		xmlTarget.setTextContent(targetID);
+		xmlElement.appendChild(xmlTarget);
+		this.storeIdPair(sourceID, targetID);
+		for (String target : targetIDs) {
+			xmlTarget = this.xmlDocument.createElement(Trace.XML_TARGET_ID);
+			xmlTarget.setTextContent(target);
+			xmlElement.appendChild(xmlTarget);
+			this.storeIdPair(sourceID, target);
+		}
+		this.sourceIDToXmlElementMap.put(sourceID, xmlElement);
 	}
 
 	public boolean isTransferred(final String sourceID) {
 		return this.sourceIDToXmlElementMap.containsKey(sourceID);
-	}
-
-	private void storeIDs(final String sourceID, final String targetID) {
-		if (this.targetIDToSourceIDMap.containsKey(targetID)) {
-			this.targetIDToSourceIDMap.get(targetID).add(sourceID);
-		} else {
-			Set<String> set = new HashSet<>();
-			set.add(sourceID);
-			this.targetIDToSourceIDMap.put(targetID, set);
-		}
-		if (this.sourceIDToTargetIDMap.containsKey(sourceID)) {
-			this.sourceIDToTargetIDMap.get(sourceID).add(targetID);
-		} else {
-			Set<String> set = new HashSet<String>();
-			set.add(targetID);
-			this.sourceIDToTargetIDMap.put(sourceID, set);
-		}
 	}
 
 	public List<String> getSourceIDs(final String targetID) {
@@ -227,25 +182,23 @@ public class Trace {
 		return new ArrayList<>(this.sourceIDToTargetIDMap.keySet());
 	}
 
-	private String getSourceID(Node element) {
-		if (element == null || element.getNodeType() != Node.ELEMENT_NODE
-				|| !((Element) element).getTagName().equals(Trace.XML_ELEMENT)) {
-			return null;
-		}
-		for (Node node = element.getFirstChild(); node != null; node = node
-				.getNextSibling()) {
-			if (node.getNodeType() == Node.ELEMENT_NODE
-					&& ((Element) node).getTagName()
-							.equals(Trace.XML_SOURCE_ID)) {
-				return node.getTextContent();
-			}
-		}
-		return null;
-	}
-
 	public String getParentID(final String sourceID) {
 		return getSourceID(this.sourceIDToXmlElementMap.get(sourceID)
 				.getParentNode());
+	}
+
+	public List<String> getTargetIDs(final String soureID) {
+		Collection<String> c = this.sourceIDToTargetIDMap.get(soureID);
+		if (c == null || c.size() == 0) {
+			return null;
+		}
+		return new ArrayList<>(c);
+	}
+
+	public void moveElement(final String sourceID, final String parentID) {
+		Element xmlElement = this.sourceIDToXmlElementMap.get(sourceID);
+		Element parent = this.sourceIDToXmlElementMap.get(parentID);
+		parent.appendChild(xmlElement);
 	}
 
 	public void removeElement(final String sourceID) {
@@ -265,9 +218,56 @@ public class Trace {
 		}
 	}
 
-	public void moveElement(final String sourceID, final String parentID) {
-		Element xmlElement = this.sourceIDToXmlElementMap.get(sourceID);
-		Element parent = this.sourceIDToXmlElementMap.get(parentID);
-		parent.appendChild(xmlElement);
+	private String getSourceID(Node element) {
+		if (element == null || element.getNodeType() != Node.ELEMENT_NODE
+				|| !((Element) element).getTagName().equals(Trace.XML_ELEMENT)) {
+			return null;
+		}
+		for (Node node = element.getFirstChild(); node != null; node = node
+				.getNextSibling()) {
+			if (node.getNodeType() == Node.ELEMENT_NODE
+					&& ((Element) node).getTagName()
+							.equals(Trace.XML_SOURCE_ID)) {
+				return node.getTextContent();
+			}
+		}
+		return null;
+	}
+
+	private void storeIdPair(final String sourceID, final String targetID) {
+		if (this.targetIDToSourceIDMap.containsKey(targetID)) {
+			this.targetIDToSourceIDMap.get(targetID).add(sourceID);
+		} else {
+			Set<String> set = new HashSet<>();
+			set.add(sourceID);
+			this.targetIDToSourceIDMap.put(targetID, set);
+		}
+		if (this.sourceIDToTargetIDMap.containsKey(sourceID)) {
+			this.sourceIDToTargetIDMap.get(sourceID).add(targetID);
+		} else {
+			Set<String> set = new HashSet<String>();
+			set.add(targetID);
+			this.sourceIDToTargetIDMap.put(sourceID, set);
+		}
+	}
+
+	private void loadElementsFromTarget(final Element trace) {
+		NodeList elementList = trace.getElementsByTagName(Trace.XML_ELEMENT);
+		for (int i = 0; i < elementList.getLength(); i++) {
+			Element element = (Element) elementList.item(i);
+
+			String sourceID = this.getSourceID(element);
+			this.sourceIDToXmlElementMap.put(sourceID, element);
+
+			for (Node node = element.getFirstChild(); node != null; node = node
+					.getNextSibling()) {
+				if (node.getNodeType() == Node.ELEMENT_NODE
+						&& ((Element) node).getTagName().equals(
+								Trace.XML_TARGET_ID)) {
+					String targetID = node.getTextContent();
+					this.storeIdPair(sourceID, targetID);
+				}
+			}
+		}
 	}
 }
